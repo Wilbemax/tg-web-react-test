b8dc9037c00d0bc5d2c334556a9649ed3b8795ba
Wilbemax <max22052007@yandex.ru>
2023-08-04 21:16:28 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-// import logo from './logo.svg';
-import { useEffect } from "react";
-import "./App.css";
-const tg = window.Telegram.WebApp;
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  useEffect(() =>{
-    tg.ready()
-  }, [])
-
-  const onClose = () => {
-    tg.close();
-  };
-
   return (
     <div className="App">
-      <button onClick={onClose}>Закрыть</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
