654f2659ac49242b748900789920aa6236788841
Wilbemax <max22052007@yandex.ru>
2023-08-13 11:46:07 +0300

    main

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Button/button.css";
+import "./Button/button.css";
 
 export default function Button(props) {
   return <button {...props} className={"button" + props.className} />;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import "../Header/Header.css"
+import "./Header/Header.css"
 import Button from '../button/button'
 import { useTelegram } from '../../hooks/useTelegram'
 
 export default function Header() {
-const{user, onClose} = useTelegram()
+const {user, onClose} = useTelegram()
 
 
   return (
     <div className={"header"}>
-        <Button>Закрыть</Button>
+        <Button onClik={onClose}>Закрыть</Button>
         <span className={"userName"}>
-
+        {user.user}
         </span>
 
     </div>
