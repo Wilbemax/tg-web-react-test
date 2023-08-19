c0dede30869fa074a70cdad30bd5e80a80ea9061
Wilbemax <max22052007@yandex.ru>
2023-08-19 18:50:49 +0300

    main

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./App.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-// import Button from "./components/Button/Button";
+import Button from "./components/Button/Button";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>Отправить</button>
+      <Button className={"button"} onClick={onToggleButton}>Отправить</Button>
     </div>
   );
 }
