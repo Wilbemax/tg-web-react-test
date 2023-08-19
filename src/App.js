08ad674f88152022d1a9cbf53e2f7ff23e9cf36d
Wilbemax <max22052007@yandex.ru>
2023-08-19 22:48:08 +0300

    main

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./App.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import Button from "./components/Button/Button";
+// import Button from "./components/Button/Button";
 import { Route, Routes } from "react-router-dom";
 import ProduktList from "./components/ProduktList/ProduktList";
 import Form from "./components/Form/Form";
--- src/components/ProduktList/ProduktList.jsx
+++ src/components/ProduktList/ProduktList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './ProduktList.module.css'
+// import styles from './ProduktList.module.css'
 
 export default function ProduktList() {
   return (
