c6afcda759c85bb6bd5a8eb7bb09fb64f4c0cc3f
Wilbemax <max22052007@yandex.ru>
2023-08-19 18:42:42 +0300

    main

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button>Отправить</button>
+      <button onClick={onToggleButton}>Отправить</button>
     </div>
   );
 }
