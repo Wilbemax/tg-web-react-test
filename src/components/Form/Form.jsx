2a906eb4278f267f53ab8a2ad5bbfac1f29d7abd
Wilbemax <max22052007@yandex.ru>
2023-09-16 22:28:40 +0300

    main

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import styles from "./Form.module.css";
 import "../../App.css";
-import { TextField,styled } from "@mui/material";
-
+import { TextField, styled, Select, MenuItem } from "@mui/material";
 
 const CssTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: 'var(--tg-theme-text-color)',
+  "& label.Mui-focused": {
+    color: "var(--tg-theme-text-color)",
   },
-  
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: ' var(--tg-theme-secondary-bg-color)',
-      color: 'var(--tg-theme-text-color)'
+
+  "& .MuiOutlinedInput-root": {
+    color: "var(--tg-theme-text-color)",
+    "& fieldset": {
+      borderColor: " var(--tg-theme-secondary-bg-color)",
     },
-    '&:hover fieldset': {
-      borderColor: 'var(--tg-theme-button-color)',
-      color: 'var(--tg-theme-text-color)'
+    "&:hover fieldset": {
+      borderColor: "var(--tg-theme-button-color)",
+      color: "var(--tg-theme-text-color)",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'var(--tg-theme-button-color)',
-      color: 'var(--tg-theme-text-color)'
+    "&.Mui-focused fieldset": {
+      borderColor: "var(--tg-theme-button-color)",
+      color: "var(--tg-theme-text-color)",
     },
   },
+});
+const CssSelect = styled(Select)({
+
 });
 
 export default function Form() {
@@ -32,24 +34,26 @@ export default function Form() {
         <h1 className={styles.title}>Ваши данные</h1>
         <form action="#">
           <div className={styles.wrap}>
-          <CssTextField label="Ваше ФИО" placeholder="Введите ваше ФИО" id="custom-css-outlined-input" />
-            <div className={styles.input}>
-              <input type="Name" placeholder="Введите ваше ФИО" />
-            </div>
-            <div className={styles.input}>
-              <input type="tel" placeholder="Введите ваше номер телефона " />
-            </div>
-            <div className={styles.input}>
-              <input type="text" placeholder="Введите адрксс доставки" />
-            </div>
-            <div className={styles.input}>
-              <select name="" id="">
-                <option value=""></option>
-              </select>
-            </div>
-            <div className={styles.input}>
-              <input type="email" placeholder="Введите ваше ФИО" />
-            </div>
+            <CssTextField
+              label="Ваше ФИО"
+              placeholder="Введите ваше ФИО"
+              id="custom-css-outlined-input"
+            />
+            <CssTextField
+              label="Номер телефона"
+              placeholder="Введите ваш номер телефона"
+              id="custom-css-outlined-input"
+            />
+            <CssTextField
+              label="Ваш адрес"
+              placeholder="Введите ваш адресс доставки"
+              id="custom-css-outlined-input"
+            />
+            <CssSelect label="Выберете способ доставки">
+              <MenuItem value={'Почта России'}>Почта России</MenuItem>
+              <MenuItem value={'СДЭК'}>СДЭК</MenuItem>
+              <MenuItem value={'BoxBerry'}>BoxBerry</MenuItem>
+            </CssSelect>
           </div>
         </form>
       </div>
